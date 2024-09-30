afc059c623df9ab36034a151b1feca3abcfbd34a
alex <sytnikov.zp@gmail.com>
2024-09-30 11:28:39 +0300

    Added 'time' middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 // =====================================
 const router = require('./routers');
 const {
+  time: { getTime, showTime },
+} = require('./middlewares');
+const {
   errorHandlers: { errorHandler },
 } = require('./middlewares');
 
@@ -14,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(getTime, showTime);
 
 app.use('/api', router);
 
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,1 +1,2 @@
 module.exports.errorHandlers = require('./errorHandlers.mw');
+module.exports.time = require('./time.mw');
